cefeb6cd13a586ff55cc3179d531c59335e2d70a
aniket-sigmoid <aniketc@sigmoidanalytics.com>
2022-11-08 17:22:26 +0530

    edited queries

--- code_dfquery.py
+++ code_dfquery.py
@@ -2,7 +2,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf,col 
 from pyspark.sql.functions import udf,col,sum,avg,count,unix_timestamp
 
-
 spark = SparkSession.builder.appName ("Analytics project session").getOrCreate()
 
 #this method to put data in spark is giving errors (all columns inferred as string, though inferschema is true)
@@ -78,7 +77,7 @@ total_orders_city_wise=csv_df.groupBy("customer_city","order_purchase_year","ord
 # total_orders_city_wise.show(5, vertical= True)
 
 #Query 3, Total no of order state wise
-total_orders_state_wise=csv_df.groupBy("customer_state","order_purchase_year","order_purchase_date")\
+total_orders_state_wise = csv_df.groupBy("customer_state","order_purchase_year","order_purchase_date")\
     .agg(count("order_items_qty")\
     .alias("Total_orders"))\
     .orderBy("customer_state","order_purchase_year","order_purchase_date")
@@ -122,14 +121,14 @@ total_sales=csv_df.groupBy("order_purchase_year","order_purchase_week")\
 
 # quey 2, Total sales in each city
 total_sales_city_wise=csv_df.groupBy("customer_city","order_purchase_year","order_purchase_week")\
-    .agg(sum(csv_df.order_items_qty*csv_df.order_products_value+csv_df.order_freight_value)\
+    .agg(sum(csv_df.order_items_qty * csv_df.order_products_value + csv_df.order_freight_value)\
     .alias("Total_sales"))\
     .orderBy("customer_city","order_purchase_year","order_purchase_week")
 # total_sales_city_wise.show(5)
 
 #quey 3, Total sales in each state
 total_sales_state_wise=csv_df.groupBy("customer_state","order_purchase_year","order_purchase_week")\
-    .agg(sum(csv_df.order_items_qty*csv_df.order_products_value+csv_df.order_freight_value)\
+    .agg(sum(csv_df.order_items_qty * csv_df.order_products_value + csv_df.order_freight_value)\
     .alias("Total_sales"))\
     .orderBy("customer_state","order_purchase_year","order_purchase_week")
 # total_sales_state_wise.show(5)
@@ -180,10 +179,10 @@ avg_time_to_approve=csv_df.groupBy("order_purchase_year","order_purchase_week")\
 
 #Query 7, AVG time taken to deliver the order
 avg_order_delivery_time=csv_df.groupBy("order_purchase_year","order_purchase_week")\
-    .agg(avg((unix_timestamp(csv_df.order_delivered_customer_date)-unix_timestamp(csv_df.order_purchase_timestamp))/24*3600)\
+    .agg(avg((unix_timestamp(csv_df.order_delivered_customer_date) - unix_timestamp(csv_df.order_purchase_timestamp))/24*3600)\
     .alias("avg_order_delivery_time_in_day"))\
     .orderBy("order_purchase_year","order_purchase_week")
-# avg_order_delivery_time.show(5)
+# avg_order_delivery_time.show(5, truncate= False)
 
 #Query c, Total freight charges
 total_freight_charges=csv_df.groupBy("order_purchase_year","order_purchase_week")\
@@ -200,8 +199,8 @@ total_freight_charges_city_wise=csv_df.groupBy("customer_city","order_purchase_y
 # total_freight_charges_city_wise.show(5)
 
 
-#  Exporting data from Queries to NO SQLDB
 
+#  Exporting data from Queries to NO SQLDB
 
 # csv_df.write\
 #     .format('com.mongodb.spark.sql.DefaultSource')\
@@ -211,4 +210,5 @@ total_freight_charges_city_wise=csv_df.groupBy("customer_city","order_purchase_y
 
 
 # write files (spark df) to amazon s3
-csv_df.write.json("s3a://bucket-name/amazon-mock-project",mode="overwrite")
+
+# csv_df.write.json("s3a://bucket-name/amazon-mock-project",mode="overwrite")
